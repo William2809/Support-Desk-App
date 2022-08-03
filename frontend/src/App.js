a1d0f2adffce5f7b112481526b7cb700bb55c214
William Huang <william.huang001@binus.ac.id>
2022-08-03 20:10:38 +0700

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import NewTicket from './pages/NewTicket';
+import PrivateRoute from './components/PrivateRoute';
+
 
 function App() {
   return (
@@ -16,9 +20,12 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
-      </Router>
+      </Router >
 
       <ToastContainer
         position="top-right"
@@ -31,7 +38,7 @@ function App() {
         draggable
         pauseOnHover
       />
-    </div>
+    </div >
   );
 }
 
